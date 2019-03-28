abb48f1cb187d96bfd3513dabf49c1fcf068e588
ŠKALDA Daniel <daniel.skalda@cns.cz>
2019-03-28 14:53:59 +0100

    Add animate.css library and use it;

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,13 @@
         <transition name="fade" appear>
           <div class="alert alert-info" v-if="show">This is some info</div>
         </transition>
+        <transition 
+                appear
+                enter-active-class="animated bounce"
+                leave-active-class="animated shake"
+            >
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
       </div>
     </div>
   </div>
@@ -32,57 +39,56 @@ export default {
 </script>
 
 <style>
-    .fade-enter {
-        opacity: 0;
-    }
+.fade-enter {
+  opacity: 0;
+}
 
-    .fade-enter-active {
-        transition: opacity 1s;
-    }
+.fade-enter-active {
+  transition: opacity 1s;
+}
 
-    .fade-leave {
-    }
+.fade-leave {
+}
 
-    .fade-leave-active {
-        transition: opacity 1s;
-        opacity: 0;
-    }
+.fade-leave-active {
+  transition: opacity 1s;
+  opacity: 0;
+}
 
-    .slide-enter {
-        opacity: 0;
-        /*transform: translateY(20px);*/
-    }
+.slide-enter {
+  opacity: 0;
+  /*transform: translateY(20px);*/
+}
 
-    .slide-enter-active {
-        animation: slide-in 1s ease-out forwards;
-        transition: opacity .5s;
-    }
+.slide-enter-active {
+  animation: slide-in 1s ease-out forwards;
+  transition: opacity 0.5s;
+}
 
-    .slide-leave {
+.slide-leave {
+}
 
-    }
+.slide-leave-active {
+  animation: slide-out 1s ease-out forwards;
+  transition: opacity 1s;
+  opacity: 0;
+}
 
-    .slide-leave-active {
-        animation: slide-out 1s ease-out forwards;
-        transition: opacity 1s;
-        opacity: 0;
-    }
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
 
-    @keyframes slide-in {
-        from {
-            transform: translateY(20px);
-        }
-        to {
-            transform: translateY(0);
-        }
-    }
-
-    @keyframes slide-out {
-        from {
-            transform: translateY(0);
-        }
-        to {
-            transform: translateY(20px);
-        }
-    }
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
+}
 </style>
